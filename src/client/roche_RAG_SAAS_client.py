e46464d71e11e11cffdf0d23339d39fc33aabd05
renhaixiang <haixiang.ren@roche.com>
2025-11-17 21:50:03 +0800

    revise RAAS

--- src/client/roche_RAG_SAAS_client.py
+++ src/client/roche_RAG_SAAS_client.py
@@ -243,8 +243,8 @@ class Roche_RAG_SAAS_client:
           ]
         }
     """
-    def list_indexed_files_in_collections(self):
-        files_url = f"{self.base_url}/files"
+    def list_indexed_files_in_collections(self,collection_id):
+        files_url = f"{self.base_url}/{collection_id}/files"
         files_response = requests.get(
             url=files_url,
             headers=self.headers)
